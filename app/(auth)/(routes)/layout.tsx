99eff1e8f01e2ea72218e78542884844ff07f720
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2024-04-08 16:15:54 +0530

    Update layout.tsx

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,22 +1,7 @@
-import React from 'react';
-
-interface AuthLayoutProps {
-  children: React.ReactNode;
-}
-
-const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return ( 
-    <div className="h-full flex items-center justify-center relative">
-      {/* Background video */}
-      <video className="absolute inset-0 w-full h-full object-cover" autoPlay loop muted>
-        <source src="/bgvv.mp4" type="video/mp4" />
-        {/* You can add more <source> tags for different video formats */}
-      </video>
-      
-      {/* Content */}
-      <div className="relative z-10">
-        {children}
-      </div>
+    <div className="h-full flex items-center justify-center">
+      {children}
     </div>
    );
 }
