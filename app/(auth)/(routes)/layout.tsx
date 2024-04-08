2b286fcbda0c21787ceec7d13d49c4f488d6aeee
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2024-04-08 20:21:02 +0530

    Update layout.tsx

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,9 +1,19 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return ( 
-    <div className="h-full flex items-center justify-center">
-      {children}
+    <div className="h-full flex items-center justify-center relative">
+      {/* Background video */}
+      <video autoPlay muted loop className="absolute inset-0 z-0 w-full h-full object-cover">
+        <source src="/bgvv.mp4" type="video/mp4" />
+        {/* Add additional sources for different video formats if needed */}
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Content */}
+      <div className="z-10">
+        {children}
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default AuthLayout;
